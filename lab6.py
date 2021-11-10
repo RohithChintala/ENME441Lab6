dc7f6fc72a0a07ffeb5f1e1cdcfda4dc049e1bde
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-11-10 21:34:50 +0000

    w

--- lab6.py
+++ lab6.py
@@ -1,26 +1,42 @@
 import time
 from led_display import LED8x8
 import multiprocessing
+import random
+
+
 
 dataPin, latchPin, clockPin = 13, 19, 26
 
 pattern = [ 
-  0b00111100, 
-  0b01000010,
-  0b10100101,
-  0b10000001,
-  0b10100101,
-  0b10011001,
-  0b01000010,
-  0b00111100] 
+  0b10000000, 
+  0b01000000,
+  0b00100000,
+  0b00010000,
+  0b00001000,
+  0b00000100,
+  0b00000010,
+  0b00000001] 
 
 #theLEDdisplay= LEDdisplay(dataPin, latchPin, clockPin)
 LED= LED8x8(dataPin, latchPin, clockPin)
+ay = 0
+ax = 0
 while True:
-  for n in range(8):
-    a = multiprocessing.Array('i',8)
-    a[n] = pattern[n]
-    p = multiprocessing.Process(name='myname',target=LED.display(n, a),args=(n, a))
-    p.daemon = True
-    p.start()
-    time.sleep(0.001)
+  x = random.randint(-1, 1)
+  y = random.randint(-1, 1)
+  ax += x
+  ay += y
+  if ay < 0:
+    ay = 7
+  if ay > 7:
+    ay = 0
+  if ax < 0:
+    ax = 7
+  if ax > 7:
+    ax = 0
+  a = multiprocessing.Array('i',8)
+  a[ay] = pattern[ay]
+  p = multiprocessing.Process(name='myname',target=LED.display(n, a),args=(n, a))
+  p.daemon = True
+  p.start()
+  time.sleep(0.1)
--- main.py
+++ main.py
@@ -1,20 +1,2 @@
-import multiprocessing
-import time
-class Countdown(multiprocessing.Process):
-  def __init__(self, count, test):
-    multiprocessing.Process.__init__(self, name=test)
-    self.count = count
-  def run(self):
-    print("Process started")
-    for i in self.count: 
-      time.sleep(0.5)
-      print(i)
-    print("Process ended")
-
-for i in range(3):
-  p = Countdown([3,2,1], "name="+str(i))
-  p.start()
-  print('p.is_alive() =', p.is_alive())
-  print(p.name)
-  p.join()
-  print('p.is_alive() =', p.is_alive())
+import random
+print(random.randint(-1, 1))
