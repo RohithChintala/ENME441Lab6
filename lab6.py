d97c0fc46cfb9b6e06c32907baf1d9a14220ac69
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-11-15 20:24:18 +0000

    h

--- lab6.py
+++ lab6.py
@@ -37,6 +37,8 @@ while True:
             h = 1
     else:
       h = 0
+      ax -= x
+      ay -= y
   f = g << abs(8-ax)
   e = ~f & mask
   a = multiprocessing.Array('i',8) #maybe instead of i have s
