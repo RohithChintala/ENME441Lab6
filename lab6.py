ceb60de977cb20e67ba8110ace8d92cdff3f5e09
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-11-16 21:06:23 +0000

    s

--- lab6.py
+++ lab6.py
@@ -1,11 +1,12 @@
 import time
 from LED8x8 import LED8x8
+from LED8x8Copy import LED8x8Copy
 import multiprocessing
 import random
 
 dataPin, latchPin, clockPin = 13, 19, 26
 
-LED= LED8x8(dataPin, latchPin, clockPin)
+#LED= LED8x8(dataPin, latchPin, clockPin)
 ay = 5
 ax = 5
 g = 0b1
@@ -31,7 +32,8 @@ while True:
   e = ~f & mask
   a = multiprocessing.Array('i',8)
   a[ay] = e
-  p = multiprocessing.Process(name='myname',target=LED.display(ay, a),args=(ay, a))
+  p = LED8x8Copy(dataPin,latchPin,clockPin, ay, a)
+  #p = multiprocessing.Process(name='myname',target=LED.display(ay, a),args=(ay, a))
   p.daemon = True
   p.start()
   time.sleep(0.1)
