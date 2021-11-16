5593a1a4599061b83da41f0a99276e8528a5cd76
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-11-16 22:21:09 +0000

    s

--- LED8x8Copy.py
+++ LED8x8Copy.py
@@ -5,7 +5,7 @@ import multiprocessing
 class LED8x8Copy(multiprocessing.Process):
   def __init__(self, data, latch, clock, num, a):
     self.shifter = Shifter(data, latch, clock)
-    multiprocessing.Process.__init__(self.display)
+    multiprocessing.Process.__init__(target=self.display)
   def display(self, num, a):
     self.shifter.shiftByte(a[num])
     self.shifter.shiftByte(1 << (num))
